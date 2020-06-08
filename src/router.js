659fe4a7e51862be2deeec403da93c211a5fa124
atongmu <736995064>
2020-06-08 13:56:30 +0800

    嗯

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import VueRouter from 'vue-router'  //导入vue-router包-01
+import VueRouter from 'vue-router' //导入vue-router包-01
 
 //导入对应的路由组件
 import HomeContainer from './components/tabbar/HomeContainer.vue'
@@ -7,7 +7,7 @@ import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
 import MemberContainer from './components/tabbar/MemberContainer.vue'
 
 //导入首页的路由组件
-import NewsList from './components/news/NewsList.vue'//新闻
+import NewsList from './components/news/NewsList.vue' //新闻
 import NewsInfo from './components/news/NewsInfo.vue'
 //导入图片分享的路由组件
 import Photolist from './components/photos/Photolist.vue'
@@ -21,23 +21,23 @@ import GoodsComment from './components/goods/GoodsComment.vue'
 
 
 //-03 创建路由对象
-var router = new VueRouter({ 
-    routes: [   //配置路由规则
-        { path: '/', redirect: '/home' },
-        { path: '/home', component: HomeContainer },
-        { path: '/search', component: SearchContainer },
-        { path: '/shopcar', component: ShopcarContainer },
-        { path: '/member', component: MemberContainer },
-        { path: '/home/newslist', component: NewsList },
-        { path: '/home/newsinfo/:id', component: NewsInfo },
-        { path: '/home/photolist', component: Photolist },
-        { path: '/home/photoinfo/:id', component: PhotoInfo },
-        { path: '/home/goodslist', component: GoodsList },
-        { path: '/home/goodsinfo/:id', component: GoodsInfo },
-        { path: '/home/goodsdesc/:id', component: GoodsDesc },
-        { path: '/home/goodscomment/:id', component: GoodsComment },
-    ],
-    linkActiveClass: 'mui-active'
+var router = new VueRouter({
+  routes: [ //配置路由规则
+    { path: '/', redirect: '/home' },
+    { path: '/home', component: HomeContainer },
+    { path: '/search', component: SearchContainer },
+    { path: '/shopcar', component: ShopcarContainer },
+    { path: '/member', component: MemberContainer },
+    { path: '/home/newslist', component: NewsList },
+    { path: '/home/newsinfo/:id', component: NewsInfo },
+    { path: '/home/photolist', component: Photolist },
+    { path: '/home/photoinfo/:id', component: PhotoInfo },
+    { path: '/home/goodslist', component: GoodsList },
+    { path: '/home/goodsinfo/:id', component: GoodsInfo },
+    { path: '/home/goodsdesc/:id', component: GoodsDesc },
+    { path: '/home/goodscomment/:id', component: GoodsComment }
+  ],
+  linkActiveClass: 'mui-active'
 })
 
 export default router
